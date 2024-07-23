2e966a83c564b884fa6fd4f8a9a6a7de8ef1d772
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-23 21:45:13 +0530

    Update No Idea.py

--- 23.07.2024/No Idea.py
+++ 23.07.2024/No Idea.py
@@ -1,1 +1,14 @@
 #  https://www.hackerrank.com/challenges/no-idea/problem?isFullScreen=true
+
+if _name_ == '_main_':
+    n,m=map(int,input().split())
+    array=list(map(int,input().split()))
+    set_A=set(map(int,input().split()))
+    set_B=set(map(int,input().split()))
+    happiness=0
+    for element in array:
+        if element in set_A:
+            happiness += 1
+        elif element in set_B:
+            happiness -= 1
+    print(happiness)
