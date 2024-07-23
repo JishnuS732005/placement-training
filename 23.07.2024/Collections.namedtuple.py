2d96c300b2c8889db7da82f421a6a0af3c994c31
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-23 21:44:23 +0530

    Update Collections.namedtuple.py

--- 23.07.2024/Collections.namedtuple.py
+++ 23.07.2024/Collections.namedtuple.py
@@ -1,1 +1,12 @@
 #  https://www.hackerrank.com/challenges/py-collections-namedtuple/problem?isFullScreen=true
+
+from collections import namedtuple
+import sys
+input = sys.stdin.read
+data=input().strip().split('\n')
+n=int(data[0])
+header=data[1].split()
+Student=namedtuple('Student',header)
+marks=[int(Student._make(line.split()).MARKS)for line in data[2:]]
+
+print(f"{sum(marks)/len(marks):.2f}")
