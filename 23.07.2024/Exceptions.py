3e8b523671c6cd0e0884453f3cee9e6622d46a62
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-23 21:43:54 +0530

    Update Exceptions.py

--- 23.07.2024/Exceptions.py
+++ 23.07.2024/Exceptions.py
@@ -1,1 +1,12 @@
 #  https://www.hackerrank.com/challenges/exceptions/problem?isFullScreen=true
+
+t=int(input().strip())
+for _ in range(t):
+    try:
+        a,b=input().split()
+        result=int(a)//int(b)
+        print(result)
+    except ZeroDivisionError as e:
+        print("Error Code:",e)
+    except ValueError as e:
+        print("Error Code:",e)
