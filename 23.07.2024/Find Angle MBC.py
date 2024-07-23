be8e53efecb96d2dec55a64168ab8b3ba29f1335
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-23 21:44:52 +0530

    Update Find Angle MBC.py

--- 23.07.2024/Find Angle MBC.py
+++ 23.07.2024/Find Angle MBC.py
@@ -1,1 +1,9 @@
 #  https://www.hackerrank.com/challenges/find-angle/problem?isFullScreen=true
+
+import math
+a=int(input().strip())
+b=int(input().strip())
+angle_radians=math.atan2(a,b)
+angle_degrees=math.degrees(angle_radians)
+angle_rounded=round(angle_degrees)
+print(f"{angle_rounded}\u00B0")
