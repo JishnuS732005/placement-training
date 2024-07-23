10b51999b3d44cf48f6bc211daa522aea4847087
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-23 21:43:30 +0530

    Update Calendar Module.py

--- 23.07.2024/Calendar Module.py
+++ 23.07.2024/Calendar Module.py
@@ -1,2 +1,7 @@
 #  https://www.hackerrank.com/challenges/calendar-module/problem?isFullScreen=true
 
+import calendar
+month,day,year=map(int,input().split())
+day_of_week=calendar.weekday(year,month,day)
+day=["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY","SUNDAY"]
+print(day[day_of_week])
