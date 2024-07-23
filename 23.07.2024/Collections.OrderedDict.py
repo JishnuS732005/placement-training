c57eaedcb19fca3098e8e19e3ffcc5ec7881ebe2
JishnuS732005 <145823703+JishnuS732005@users.noreply.github.com>
2024-07-23 21:45:35 +0530

    Update Collections.OrderedDict.py

--- 23.07.2024/Collections.OrderedDict.py
+++ 23.07.2024/Collections.OrderedDict.py
@@ -1,1 +1,15 @@
 #  https://www.hackerrank.com/challenges/py-collections-ordereddict/problem?isFullScreen=true
+
+from collections import OrderedDict
+n=int(input())
+item_prices=OrderedDict()
+for _ in range(n):
+    entry=input().rsplit(' ',1)
+    item=entry[0]
+    price=int(entry[1])
+    if item in item_prices:
+        item_prices[item]+=price
+    else:
+        item_prices[item]=price
+for item,net_price in item_prices.items():
+    print(f"{item} {net_price}")
